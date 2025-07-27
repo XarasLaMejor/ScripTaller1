359a81d9afd7241699dd692909e75d315111a494
marianaec19 <143814482+marianaec19@users.noreply.github.com>
2025-07-27 18:57:56 -0500

    Add files via upload

---
+++ EjerciciosTaller1/Ejercicio1-RaizDigital/Actividad-Grupal.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i % 2 == 0)
+                Console.WriteLine($"{i} es par");
+            else
+                Console.WriteLine($"{i} es impar");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
